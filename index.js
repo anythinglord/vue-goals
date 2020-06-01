7982eb7d0967c5cdc7fbda5b8aca7916e9a96e2d
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2020-06-01 17:02:22 -0500

    first directives!

---
+++ index.js
@@ -0,0 +1,36 @@
+var app = new Vue({
+    el: '#vueGoalApp',
+    data:{
+        message: 'Hello Vue!',
+        isTestDisplayed: false,
+        titleMessage: 'Title Message Vue!!!!!',
+        user: {
+            name: 'Pepito perez',
+            id: '-Aj34jknvncx98812',
+        },
+        goals: {
+            '1546968934': {
+                id: '1546968934',
+                title: 'Learn Vue.js',
+                notes: 'I started today and it was not good.',
+                progress: 0,
+                category: '1546969049',
+                createdAt: 1546969144391,
+                updatedAt: 1546969144391
+            },
+            '1546969212': {
+                id: '1546969212',
+                title: 'Read Witcher Books',
+                notes: 'These books are super nice',
+                progress: 0,
+                category: '1546969049',
+                createdAt: 1546969144391,
+                updatedAt: 1546969144391
+            }
+        },
+        categories: {
+            '1546969049': {text: 'books'},
+            '1546969225': {text: 'movies'}
+        }
+    }
+})
